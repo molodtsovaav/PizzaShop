74a25882792a6de731d6a65c4fd59c0eeac5557e
molodtsovaav <molodtsovaav@gmail.com>
2019-08-15 11:47:41 +0400

    Add add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,9 +1,11 @@
-
-
 function something() {
     var x = window.localStorage.getItem('aaa');
     x = x * 1 + 1;
     window.localStorage.setItem('aaa', x);
     alert(x);
 
+}
+
+function add_to_cart() {
+    alert('Hello from function');
 }
