ca8d8f1be35ec398221ea3349d81bb6cbad1b322
molodtsovaav <molodtsovaav@gmail.com>
2019-08-15 13:46:27 +0400

    Add cart element to navbar

--- public/app.js
+++ public/app.js
@@ -12,6 +12,9 @@ function add_to_cart(id) {
    var x = window.localStorage.getItem(key);
    x = x * 1 + 1;
    window.localStorage.setItem(key, x);
+
+   alert('Items i your cart: ' + cart_get_number_of_items());
+
 }
 
 function cart_get_number_of_items() {
